1c0fa17a95f344b53f48187292d3ed8909ff193c
Tomasz Cichocki <cichymail@gmail.com>
2022-10-19 22:49:24 +0200

    luacheck settings

--- talkie.lua
+++ talkie.lua
@@ -26,15 +26,12 @@ local function decode16(s)
 end
 
 -- CONSTANTS
-local chirp = decode8("\0*\212\50\178\18%\20\2\225\197\2_Z\5\15&\252\165\165\214\221\220\252%+\34!\15\255"..
-	"\248\238\237\239\247\246\250\0\3\2\1")
+-- luacheck: push no max line length
+local chirp = decode8("\0*\212\50\178\18%\20\2\225\197\2_Z\5\15&\252\165\165\214\221\220\252%+\34!\15\255\248\238\237\239\247\246\250\0\3\2\1")
 local tmsEnergy = decode8e("\0\2\3\4\5\7\10\15\20 )\57Qr\161") -- 0 - 0.62890625 / 2
-local tmsPeriod = decode8p("\0\16\17\18\19\20\21\22\23\24\25\26\27\28\29\30\31 !\34#$%&'()*+-/\49\51\53\54\57;=?BEGIM"..
-	"OQUW\92_cfjnsw{\128\133\138\143\149\154\160")
-local tmsK1 = decode16("\192\130\128\131\192\131@\132\192\132@\133\0\134\128\135\128\136\128\137\192\138\0\140@\141\0"..
-	"\143\192\144\192\146\0\153@\161\128\171@\184@\199\192\216\192\235\0\0@\20@'\192\56\192G\128T\192^\0g@m")
-local tmsK2 = decode16("\0\174\128\180\128\187@\195\128\203@\212\192\221\128\231\128\241\192\251\0\6@\16@\26\0$@-\0"..
-	"\54@>\192E\192L\0S\128X\192]@b@f\192i\192l\128o\192q\192s\128u\0w\128~")
+local tmsPeriod = decode8p("\0\16\17\18\19\20\21\22\23\24\25\26\27\28\29\30\31 !\34#$%&'()*+-/\49\51\53\54\57;=?BEGIMOQUW\92_cfjnsw{\128\133\138\143\149\154\160")
+local tmsK1 = decode16("\192\130\128\131\192\131@\132\192\132@\133\0\134\128\135\128\136\128\137\192\138\0\140@\141\0\143\192\144\192\146\0\153@\161\128\171@\184@\199\192\216\192\235\0\0@\20@'\192\56\192G\128T\192^\0g@m")
+local tmsK2 = decode16("\0\174\128\180\128\187@\195\128\203@\212\192\221\128\231\128\241\192\251\0\6@\16@\26\0$@-\0\54@>\192E\192L\0S\128X\192]@b@f\192i\192l\128o\192q\192s\128u\0w\128~")
 local tmsK3 = decode8("\146\159\173\186\200\213\227\240\254\11\25&\52AO\92")
 local tmsK4 = decode8("\174\188\202\216\230\244\1\15\29+\57GUcq~")
 local tmsK5 = decode8("\174\186\197\209\221\232\244\255\11\23\34.\57EQ\92")
@@ -43,6 +40,7 @@ local tmsK7 = decode8("\179\191\203\215\227\239\251\7\19\31+\55COZf")
 local tmsK8 = decode8("\192\216\240\7\31\55Of")
 local tmsK9 = decode8("\192\212\232\252\16%\57M")
 local tmsK10 = decode8("\205\223\241\4\22 ;M") -- -0.3984375 - 0.6015625
+-- luacheck: pop
 local rate = 8000 -- samples per second
 local ticks = rate / 40
 
@@ -113,6 +111,12 @@ local function doSynth(soundData, bits)
 						synthK8 = tmsK8[bits:get(3)+1]
 						synthK9 = tmsK9[bits:get(3)+1]
 						synthK10 = tmsK10[bits:get(3)+1]
+					else
+						x9 = 0
+						x8 = 0
+						x7 = 0
+						x6 = 0
+						x5 = 0
 					end
 				end
 			end
@@ -133,16 +137,18 @@ local function doSynth(soundData, bits)
 				u0 = -synthEnergy
 			end
 		end
-		u0 = u0 - synthK10*x9 + synthK9*x8
-		x9 = x8 + synthK9*u0
-		u0 = u0 - synthK8*x7
-		x8 = x7 + synthK8*u0
-		u0 = u0 - synthK7*x6
-		x7 = x6 + synthK7*u0
-		u0 = u0 - synthK6*x5
-		x6 = x5 + synthK6*u0
-		u0 = u0 - synthK5*x4
-		x5 = x4 + synthK5*u0
+		if synthPeriod ~= 0 then
+			u0 = u0 - synthK10*x9 + synthK9*x8
+			x9 = x8 + synthK9*u0
+			u0 = u0 - synthK8*x7
+			x8 = x7 + synthK8*u0
+			u0 = u0 - synthK7*x6
+			x7 = x6 + synthK7*u0
+			u0 = u0 - synthK6*x5
+			x6 = x5 + synthK6*u0
+			u0 = u0 - synthK5*x4
+			x5 = x4 + synthK5*u0
+		end
 		u0 = u0 - synthK4*x3
 		x4 = x3 + synthK4*u0
 		u0 = u0 - synthK3*x2
@@ -162,7 +168,7 @@ end
 
 function talkie.synth(bitString)
     local bits = bitstream.Bits:new(bitString)
-    local length    = getFrameCount(bits)*ticks
+    local length = getFrameCount(bits)*ticks
     print("length is "..(length/rate).."s")
     local soundData = love.sound.newSoundData(length, rate, 16, 1)
     doSynth(soundData, bits)
